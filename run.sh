57c9664049e837df8d8063dc815956bd6bddf07c
Jean Mertz <jean@mertz.fm>
2016-06-05 00:44:10 +0200

    use printf to print message

--- run.sh
+++ run.sh
@@ -10,7 +10,7 @@ main() {
   fi
 
   if [ "$WERCKER_PUBLISH_QUAY_TAG_IF" != "$WERCKER_PUBLISH_QUAY_TAG_EQ" ]; then
-    info "$WERCKER_PUBLISH_QUAY_TAG_IF did not match $WERCKER_PUBLISH_QUAY_TAG_EQ, skipping step..."
+    printf "%b%b%b\n" "[37m[1m" "$WERCKER_PUBLISH_QUAY_TAG_IF did not match $WERCKER_PUBLISH_QUAY_TAG_EQ, skipping step..." "[m"
   else
     imageID=$(curl --silent --header "Authorization: Bearer $WERCKER_PUBLISH_QUAY_TAG_TOKEN" "https://quay.io/api/v1/repository/$WERCKER_PUBLISH_QUAY_TAG_REPOSITORY/tag/$WERCKER_PUBLISH_QUAY_TAG_CURRENT/images" | awk -F'"id": "' '{print $2}' | awk -F'"' '{print $1}')
 
